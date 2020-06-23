b47d92466502a4c5749e28a301413174011f792b
alfatlaloc <mr.alfatlaloc@gmail.com>
2020-06-22 22:47:29 -0500

    akaka

--- actions.js
+++ actions.js
@@ -1,6 +1,6 @@
 function randomPage() {
     let auxS = window.location.href;
     let str = auxS.substring(0, auxS.length-6);
-    window.location.href = `/${1 + Math.floor(Math.random() * 40)}.html`; 
+    window.location.href = `${1 + Math.floor(Math.random() * 40)}.html`; 
   }
 
